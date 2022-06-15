6ad779824ea074c654b06013f158df2c2a4743e3
Jasper Timm <jaspa@jaspa.codes>
2022-06-15 14:53:34 +0100

    Small script changes from testing

--- ethereum/kycdao-ntnft/scripts/upgrade.ts
+++ ethereum/kycdao-ntnft/scripts/upgrade.ts
@@ -2,13 +2,13 @@ const { ethers, upgrades } = require('hardhat')
 import { ContractFactory } from '@ethersproject/contracts'
 import { KycdaoNTNFT } from '../src/types/contracts/KycdaoNTNFT'
 
-const PROXY_ADDRESS = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+const PROXY_ADDRESS = "0x0B306BF915C4d645ff596e518fAf3F9669b97016"
 
 async function main() {
-  const KycdaoNTNFTContract = (await ethers.getContractFactory('KycdaoNTNFT')) as ContractFactory
+  const KycdaoNTNFTAccredContract = (await ethers.getContractFactory('KycdaoNTNFTAccreditation')) as ContractFactory
 
-  console.log('Upgrading KycdaoNTNFT...')
-  await upgrades.upgradeProxy(PROXY_ADDRESS, KycdaoNTNFTContract)
+  console.log('Upgrading KycdaoNTNFT to KycdaoNTNFTAccredContract...')
+  await upgrades.upgradeProxy(PROXY_ADDRESS, KycdaoNTNFTAccredContract)
   console.log('KycdaoNTNFT upgraded')
 }
 
