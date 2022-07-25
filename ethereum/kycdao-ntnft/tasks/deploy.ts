fa4a5966fb50307daa45386187c94263081e06cf
Jasper Timm <jaspa@jaspa.codes>
2022-07-25 18:33:28 +0100

    Temp fixes for Polygon deployment

--- ethereum/kycdao-ntnft/tasks/deploy.ts
+++ ethereum/kycdao-ntnft/tasks/deploy.ts
@@ -17,53 +17,61 @@ task("deploy", "Deploys the proxy and impl contract (using xdeploy) to a network
     .setAction(async ({ contract, salt }, hre) => {
         //Deploy implementation contract    
         const implContract = (await hre.ethers.getContractFactory(contract)) as ContractFactory
-        console.log(`Deploying implementation: ${contract}, to network: ${hre.network.name}...`)
-        const deployedImpl = await implContract.deploy()
-        console.log(`Implementation deployed at: ${deployedImpl.address}`)
+        // console.log(`Deploying implementation: ${contract}, to network: ${hre.network.name}...`)
+        // const deployedImpl = await implContract.deploy()
+        // console.log(`Started deployment tx: ${deployedImpl.deployTransaction.hash}, waiting for confirmations...`)
+        // console.log({
+        //     gasPrice: deployedImpl.deployTransaction.gasPrice,
+        //     gasLimit: deployedImpl.deployTransaction.gasLimit,
+        //     nonce: deployedImpl.deployTransaction.nonce,
+        //     address: deployedImpl.address
+        // })
+        // await deployedImpl.deployTransaction.wait(1)
+        // console.log(`Implementation deployed at: ${deployedImpl.address}`)
 
         //Deploy proxy for implementation using xdeploy
-        console.log(`Deploying proxy for implementation using xdeploy...`)
-        const networkConf = hre.network.config as HttpNetworkConfig
-        const netAccts = networkConf.accounts as HttpNetworkHDAccountsConfig
-        hre.config.xdeploy = {
-            contract: "ProxyUUPS",
-            // constructorArgsPath: "./emptyArgs.js",
-            salt: salt,
-            signer: privateKey(netAccts.mnemonic),
-            networks: [hre.network.name],
-            rpcUrls: [networkConf.url],
-            gasLimit: 1.2 * 10 ** 6,            
-        }
-        await hre.run('xdeploy')
+        // console.log(`Deploying proxy for implementation using xdeploy...`)
+        // const networkConf = hre.network.config as HttpNetworkConfig
+        // const netAccts = networkConf.accounts as HttpNetworkHDAccountsConfig
+        // hre.config.xdeploy = {
+        //     contract: "ProxyUUPS",
+        //     // constructorArgsPath: "./emptyArgs.js",
+        //     salt: salt,
+        //     signer: privateKey(netAccts.mnemonic),
+        //     networks: [hre.network.name],
+        //     rpcUrls: [networkConf.url],
+        //     gasLimit: 1.2 * 10 ** 6,
+        // }
+        // await hre.run('xdeploy')
 
-        const xdeployPath = path.normalize(
-            path.join(
-              hre.config.paths.root,
-              "deployments",
-              `${hre.network.name}_deployment.json`
-            )
-          )
-        const xdeployResult = require(xdeployPath)
-        if (!xdeployResult.deployed) {
-            console.error("Proxy deployment failed! See xdeploy logs for details")
-            return
-        }
+        // const xdeployPath = path.normalize(
+        //     path.join(
+        //       hre.config.paths.root,
+        //       "deployments",
+        //       `${hre.network.name}_deployment.json`
+        //     )
+        //   )
+        // const xdeployResult = require(xdeployPath)
+        // if (!xdeployResult.deployed) {
+        //     console.error("Proxy deployment failed! See xdeploy logs for details")
+        //     return
+        // }
 
         // Set the proxy to the new implementation
         console.log('Setting proxy to implementation and running initialize...')
         const proxyContractAbstract = (await hre.ethers.getContractFactory('ProxyUUPS')) as ContractFactory
-        const proxyContract = proxyContractAbstract.attach(xdeployResult.address) as ProxyUUPS
+        const proxyContract = proxyContractAbstract.attach('0xdf1fc8279c15746450f005cf6ae7992d15b08755') as ProxyUUPS
         const initData = implContract.interface.encodeFunctionData('initialize', [args.name, args.symbol, args.baseURI, args.verificationBaseURI])
-        const tx = await proxyContract.initProxy(deployedImpl.address, initData)
+        const tx = await proxyContract.initProxy('0x94990696c95a1f10d27391ab5d37cb8b3172c186', initData)
         console.log('Done')
 
-        // Verify implementation source
-        console.log('Verifying source...')
-        console.log('Waiting for 5 confirmations...')
-        await tx.wait(5)
-        await hre.run("verify:verify", {address: deployedImpl.address})
+        // // Verify implementation source
+        // console.log('Verifying source...')
+        // console.log('Waiting for 5 confirmations...')
+        // await tx.wait(5)
+        // await hre.run("verify:verify", {address: deployedImpl.address})
 
-        console.log('Deployment complete!')
+        // console.log('Deployment complete!')
 });
 
 module.exports = {};
