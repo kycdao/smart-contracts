46acb2635a12bb675c00d52f498b2cb6d94e5405
Jasper Timm <jaspa@jaspa.codes>
2022-11-15 16:49:54 +0000

    [DOCS] Update both EVM contracts to 0.3.2

--- ethereum/kycdao-ntnft/contracts/KycdaoNTNFT.sol
+++ ethereum/kycdao-ntnft/contracts/KycdaoNTNFT.sol
@@ -191,7 +191,7 @@ contract KycdaoNTNFT is ERC721EnumerableUpgradeable, AccessControlUpgradeable, B
 
     /// @dev Current version of this smart contract
     function version() public pure returns (string memory) {
-        return "0.3.1";
+        return "0.3.2";
     }
 
     function tokenURI(uint256 tokenId)
