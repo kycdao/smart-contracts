768e39794bf1dc003b2eb58570a0c6fcefd81947
Juhasz Sandor <blackghost1987@gmail.com>
2022-12-19 14:29:41 +0100

    near price feed mocked

--- near/kycdao-ntnft/src/price_feed.rs
+++ near/kycdao-ntnft/src/price_feed.rs
@@ -4,15 +4,28 @@ use near_sdk::AccountId;
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct PriceFeed {
     address: AccountId,
+    last_price: (u32, u8),
 }
 
-impl PriceFeed {
-    pub fn new(address: AccountId) -> PriceFeed {
-        PriceFeed { address }
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct PriceFeedMocked {
+    address: AccountId,
+    last_price: (u32, u8),
+}
+
+impl PriceFeedMocked {
+    pub fn new(address: AccountId) -> Self {
+        PriceFeedMocked {
+            address,
+            last_price: (17370, 4)
+        }
+    }
+
+    pub fn set_last_price(&mut self, price: u32, decimals: u8) {
+        self.last_price = (price, decimals);
     }
 
     pub fn last_price(&self) -> (u32, u8) {
-        //unimplemented!("TODO")
-        (17240, 4)
+        self.last_price
     }
 }
